0bbf76f5796936eaca360ddb0a8d0ad5bb36bca9
Jonte <highfestiva@gmail.com>
2020-12-16 22:30:48 +0100

    Use first word of gl_code_name for fldGrootboeknummer
    Also generate three lines per each invoice item: debit, credit and tax.

--- knastomte.py
+++ knastomte.py
@@ -12,7 +12,7 @@ import xml.etree.ElementTree as ET
 
 columns = [ 'allocation', 'invoice_index', 'invoice_no', 'invoice_date', 'pay_date', 'customer_number', 'customer_name',
             'tax_total', 'tax_total_s', 'invoice_total', 'invoice_total_s', 'allocation_code',
-            'booking_no'
+            'allocation_lookup', 'gl_code_name',
           ]
 
 
@@ -72,32 +72,40 @@ def main(options):
                 payment_date = invoice.find('ns:payment_due_date', ns).text
                 timestamp = dateutil.parser.parse(payment_date).timestamp()
                 payment_date = datetime.fromtimestamp(timestamp).isoformat().partition('T')[0]
-                tax_total = invoice.find('ns:total_tax_value', ns).text
-                tax_total = float(tax_total)
-                tax_total_s = ('%.2f'%float(tax_total)).replace('.',',')
-                invoice_total = invoice.find('ns:invoice_total', ns).text
-                invoice_gross = float(invoice_total)
-                invoice_total = invoice_gross + tax_total
-                invoice_total_s = ('%.2f'%invoice_total).replace('.',',')
+                # tax_total = invoice.find('ns:total_tax_value', ns).text
+                # tax_total = float(tax_total)
+                # tax_total_s = ('%.2f'%float(tax_total)).replace('.',',')
+                # invoice_total = invoice.find('ns:invoice_total', ns).text
+                # invoice_gross = float(invoice_total)
+                # invoice_total = invoice_gross + tax_total
+                # invoice_total_s = ('%.2f'%invoice_total).replace('.',',')
                 invoice_number = invoice.find('ns:invoice_number', ns).text
                 allocs = []
                 for invoice_item in invoice.findall('ns:invoice_item', ns):
+                    tax_total = invoice_item.find('ns:total_tax_amount', ns).text
+                    tax_total = float(tax_total)
+                    tax_total_s = '?'
+                    invoice_gross = invoice_item.find('ns:gross_amount', ns).text
+                    invoice_gross = float(invoice_gross)
+                    invoice_net = invoice_gross - tax_total
+                    print(invoice_gross, invoice_net, tax_total)
+                    invoice_total_s = '?'
+
                     allocation = invoice_item.find('ns:allocation_code_name', ns).text
-                    if allocation not in allocation_lookup:
-                        print('FATAL: no such allocation %s in allocation.cfg' % allocation)
-                        return
-                    allocation_code = allocation_lookup[allocation]
-                    allocs += [(allocation_code,allocation)]
-                allocation_code, allocation = sorted(allocs)[0]
-                table += [[allocation, invoice_index, invoice_number, invoice_date, payment_date, customer_id, customer_name, tax_total, tax_total_s, -invoice_total, invoice_total_s, allocation_code, allocation_lookup['Debiteuren']]]
-                table += [[allocation, invoice_index, invoice_number, invoice_date, payment_date, customer_id, customer_name, tax_total, tax_total_s, +invoice_gross, invoice_total_s, allocation_code, allocation_code]]
-                table += [[allocation, invoice_index, invoice_number, invoice_date, payment_date, customer_id, customer_name, tax_total, tax_total_s,     +tax_total, invoice_total_s, allocation_code, allocation_lookup['BTW']]]
+                    gl_code_name = invoice_item.find('ns:gl_code_name', ns).text.split()[0]
+                    # if allocation not in allocation_lookup:
+                        # print('FATAL: no such allocation %s in allocation.cfg' % allocation)
+                        # return
+                    # allocation_code = allocation_lookup[allocation]
+                    table += [[allocation, invoice_index, invoice_number, invoice_date, payment_date, customer_id, customer_name, tax_total, tax_total_s, -invoice_gross, invoice_total_s, 'debit',  allocation_lookup['Debiteuren'], gl_code_name]]
+                    table += [[allocation, invoice_index, invoice_number, invoice_date, payment_date, customer_id, customer_name, tax_total, tax_total_s,   +invoice_net, invoice_total_s, 'credit', allocation_lookup['fldDagboek'], gl_code_name]]
+                    table += [[allocation, invoice_index, invoice_number, invoice_date, payment_date, customer_id, customer_name, tax_total, tax_total_s,     +tax_total, invoice_total_s, 'tax',    allocation_lookup['BTW'], gl_code_name]]
                 invoice_index += 1
 
-    add_col(table, 'invoice_index',     'fldDagboek', lambda i: allocation_lookup['fldDagboek'])
+    add_col(table, 'allocation_lookup', 'fldDagboek', lambda s: s)
     add_col(table, 'invoice_index',     'fldBoekingcode', lambda s: s)
     add_col(table, 'invoice_date',      'fldDatum', lambda s: '-'.join(reversed(s.split('-'))))
-    add_col(table, 'booking_no',        'fldGrootboeknummer', lambda x: x)
+    add_col(table, 'gl_code_name',      'fldGrootboeknummer', lambda x: x)
     add_col(table, 'invoice_total',     'fldDebet', lambda x: ('%.2f'%round(-x,2) if x<0 else '0.00'))
     add_col(table, 'invoice_total',     'fldCredit', lambda x: ('%.2f'%round(x,2) if x>0 else '0.00'))
     add_col(table, 'customer_name',     'fldOmschrijving', lambda s: s)
@@ -117,8 +125,8 @@ def main(options):
             wr.writerow(row)
 
     # show we did something
-    gross_total = sum(float(row[columns.index('fldDebet')])+float(row[columns.index('fldCredit')]) for row in table if row[columns.index('booking_no')]==allocation_lookup['Debiteuren'])
-    net_total = sum(float(row[columns.index('fldDebet')])+float(row[columns.index('fldCredit')]) for row in table if row[columns.index('booking_no')] not in [allocation_lookup['Debiteuren'], allocation_lookup['BTW']])
+    gross_total = sum(float(row[columns.index('fldDebet')])+float(row[columns.index('fldCredit')]) for row in table if row[columns.index('allocation_code')]=='debit')
+    net_total = sum(float(row[columns.index('fldDebet')])+float(row[columns.index('fldCredit')]) for row in table if row[columns.index('allocation_code')]=='credit')
     print('Converted %i invoices. Gross %.2f EUR = net %.2f EUR. Written to "%s".' % (len(table)//3, gross_total, net_total, options.output_file))
 
     move_files(options.today, files)
